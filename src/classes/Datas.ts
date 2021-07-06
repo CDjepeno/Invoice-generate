081c5a07119318b81121a5a7d809f77729e63963
CDjepeno <djepeno.christopher@gmail.com>
2021-07-07 01:49:33 +0200

    fill invoice when submited

--- src/classes/Datas.ts
+++ src/classes/Datas.ts
@@ -15,37 +15,44 @@ export class Datas implements IHasHtmlFormat {
     private tva: number,
     private date: Date,
 
-  ) {
+  ) {}
 
+  private subtotal(price: number, quantity: number, tva: number): number {
+    const tvaPercent = tva / 100;
+    const totalTva = price * tvaPercent
+    const totalTtc = (price + totalTva) * quantity 
+
+    return totalTtc
   }
 
   htmlFormat() {
+    const totalPrice = this.subtotal(this.price, this.quantity, this.tva)
     return `
       <div class="row p-5">
           <div class="col-md-6">
               <h2 class="text-left">LOGO</h2>
           </div>
           <div class="col-md-6 text-right">
-              <p class="font-weight-bold mb-1">xxxxxxxxxxxxxxx<span class="font-weight-normal">xxxxxxxxxx</span></p>
-              <p class="font-weight-bold mb-1">Date <span class="font-weight-normal">xxxxxxxxxxxxxxx</span></p>
+              <p class="font-weight-bold mb-1">${this.documentType === "invoice" ? "Facture" : "devis"}<span class="font-weight-normal">N° ${Math.floor(Math.random()*101)}</span></p>
+              <p class="font-weight-bold mb-1">${this.date}<span class="font-weight-normal">${this.date.toLocaleDateString()}</span></p>
           </div>
       </div>
 
       <div class="row pb-5 p-5">
           <div class="col-sm-6 text-left">
-              <p class="font-weight-bold">Entreprise de Toto</p>
-              <p class="mb-1">22 boulevard Moe Szyslak</p>
+              <p class="font-weight-bold">Entreprise de Djepeno</p>
+              <p class="mb-1">22 Avenue Marceau</p>
               <p>75008 - Paris</p>
-              <p class="mb-1">Tél: 00.00.00.00.00</p>
+              <p class="mb-1">Tél: 01.44.33.22.00</p>
           </div>
 
           <div class="col-sm-6 text-right">
               <p class="font-weight-bold">Informations du client</p>
-              <p class="mb-1">Mr/Mme xxxxxxxxxxxxxxx</p>
-              <p class="mb-1">xxxxxxxxxxxxxxx</p>
-              <p>xxxxxxxxxxxxxxx</p>
-              <p>xxxxxxxxxxxxxxx</p>
-              <p>xxxxxxxxxxxxxxx</p>
+              <p class="mb-1">Mr/Mme ${this.lastName} ${this.lastName}</p>
+              <p class="mb-1">${this.address}</p>
+              <p>${this.zip}</p>
+              <p>${this.town}</p>
+              <p>${this.country}</p>
           </div>
       </div>
 
@@ -62,10 +69,10 @@ export class Datas implements IHasHtmlFormat {
               </thead>
               <tbody>
                   <tr>
-                  <td>xxxxxxxxxxxxxxx</td>
-                  <td>xxxxxxxxxxxxxxx € HT</td>
-                  <td>xxxxxxxxxxxxxxx</td>
-                  <td>xxxxxxxxxxxxxxx € HT</td>
+                  <td>${this.product}</td>
+                  <td>${this.price} € HT</td>
+                  <td>${this.quantity}</td>
+                  <td>${this.price * this.quantity} € HT</td>
                   </tr>
               </tbody>
               </table>
@@ -75,7 +82,7 @@ export class Datas implements IHasHtmlFormat {
       <div class="d-flex flex-row-reverse bg-light p-4">
           <div class="py-3 px-5">
               <div class="mb-2">TOTAL TTC</div>
-              <div class="h2 font-weight-light">xxxxxxxxxxxxxxx €</div>
+              <div class="h2 font-weight-light">${totalPrice} €</div>
           </div>
       </div>
     `;
