629433a510f82f09acecc9253571acc4b9ab4719
CDjepeno <djepeno.christopher@gmail.com>
2021-07-06 21:57:06 +0200

    create invoice class and index.html

--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,72 @@
-console.log('hello')
+class FormInput {
+  form: HTMLFormElement;
+  type: HTMLSelectElement;
+  firstName: HTMLInputElement;
+  lastName: HTMLInputElement;
+  address: HTMLInputElement;
+  country: HTMLInputElement;
+  town: HTMLInputElement;
+  zip: HTMLInputElement;
+  product: HTMLInputElement;
+  price: HTMLInputElement;
+  quantity: HTMLInputElement;
+  tva: HTMLInputElement;
+
+  constructor() {
+    this.form = document.getElementById("form") as HTMLFormElement;
+    this.type = document.getElementById("type") as HTMLSelectElement;
+    this.firstName = document.getElementById("firstName") as HTMLInputElement;
+    this.lastName = document.getElementById("lastName") as HTMLInputElement;
+    this.address = document.getElementById("address") as HTMLInputElement;
+    this.country = document.getElementById("country") as HTMLInputElement;
+    this.town = document.getElementById("town") as HTMLInputElement;
+    this.zip = document.getElementById("zip") as HTMLInputElement;
+    this.product = document.getElementById("product") as HTMLInputElement;
+    this.price = document.getElementById("price") as HTMLInputElement;
+    this.quantity = document.getElementById("quantity") as HTMLInputElement;
+    this.tva = document.getElementById("tva") as HTMLInputElement;
+
+    this.submitFormListener();
+  }
+
+  private submitFormListener(): void {
+    this.form.addEventListener("submit", this.handleFormSubmit.bind(this))
+  }
+
+  private handleFormSubmit(e: Event) {
+    e.preventDefault()
+
+    const inputs = this.inputDatas()
+
+    if(Array.isArray(inputs)) {
+      const [Type, firstname, lastname,address,country,town,zip, product, price, quantity, tva] = inputs;
+      return console.log(Type, firstname, lastname,address,country,town,zip, product, price, quantity, tva);
+    } 
+    alert("ce n'est pas un tableau")
+
+  }
+
+  private inputDatas(): [string, string, string,string,string,string,number, string, number, number, number] | void {
+    const Type = this.type.value;
+    const firstname = this.firstName.value;
+    const lastname = this.lastName.value;
+    const address = this.address.value;
+    const country = this.country.value;
+    const town = this.town.value;
+    const zip = this.zip.valueAsNumber;
+    const product = this.product.value;
+    const price = this.price.valueAsNumber;
+    const quantity = this.quantity.valueAsNumber;
+    const tva = this.tva.valueAsNumber;
+
+    if(zip > 0 && price > 0 && quantity > 0 && tva > 0) {
+      return [Type, firstname, lastname,address,country,town,zip, product, price, quantity, tva]
+    }
+    alert('Les valeurs numérique doivent être supérieur à 0')
+    return
+  }
+
+
+}
+
+new FormInput();
