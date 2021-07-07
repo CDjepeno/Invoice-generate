36807987a004fe4fb65bb178cfd191a11f80de18
CDjepeno <djepeno.christopher@gmail.com>
2021-07-07 03:37:42 +0200

    Create item for localStorage

--- public/script.js
+++ public/script.js
@@ -1,2 +1,5 @@
 import { FormInput } from './classes/FormInput.js';
+import { Storage } from './classes/Storage.js';
 new FormInput();
+const storage = new Storage();
+Storage.checkLocalStorage();
--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,7 @@
 import { FormInput } from './classes/FormInput.js';
-
+import { Storage } from './classes/Storage.js'
 new FormInput()
+
+const storage =  new Storage()
+
+Storage.checkLocalStorage()
